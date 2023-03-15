dc973acbb822bc6db105b6ef720b4b66f1a56ca9
amigo2208 <alekon288@gmail.com>
2023-03-15 17:21:51 +0700

    Add deployment Edit readme

--- stonefish-ps/src/environments/environment.prod.ts
+++ stonefish-ps/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: true
-  
+  production: true,
+  apiEndpoint: 'http://127.0.0.1:8080/api/v1'
 };
--- stonefish/redis_client.py
+++ stonefish/redis_client.py
@@ -1,9 +1,15 @@
 """ Module contain a redis client """
 
+import os
+
 from redis import Redis
 
 
 class RedisClient:
     """ Redis client """
     def __init__(self):
-        self.client = Redis(db=0, host='localhost', port=6379)
+        self.client = Redis(
+            db=0,
+            host=os.environ.get('REDIS_HOST'),
+            port=int(os.environ.get('REDIS_PORT'))
+        )
