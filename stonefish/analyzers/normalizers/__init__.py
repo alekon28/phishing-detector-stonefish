5969070da7d01ab906cd0cc73bf0b4a2c1144fc4
amigo2208 <alekon288@gmail.com>
2023-03-16 21:51:32 +0700

    Fix adding sigmoid normalizer

--- stonefish/analyzers/normalizers/__init__.py
+++ stonefish/analyzers/normalizers/__init__.py
@@ -3,10 +3,12 @@
 from .tanh_normalizer import ThNormalizer
 from .binary_step_normalizer import BinaryStepNormalizer
 from .gaussian_normalizer import GaussianNormalizer
+from .sigmoid_normalizer import SigmoidNormalizer
 
 
 __all__ = [
     'ThNormalizer',
     'BinaryStepNormalizer',
     'GaussianNormalizer',
+    'SigmoidNormalizer',
 ]
--- stonefish/analyzers/normalizers/sigmoid_normalizer.py
+++ stonefish/analyzers/normalizers/sigmoid_normalizer.py
@@ -1,11 +1,11 @@
-""" Sigmoid based on hyperbolic tangent function """
+""" Sigmoid based on sigmoid function """
 
 from math import exp
 
 from stonefish.analyzers.normalizers.base_normalizer import BaseNormalizer
 
 
-class ThNormalizer(BaseNormalizer):
-    """ Normalizer based on hyperbolic tangent function """
+class SigmoidNormalizer(BaseNormalizer):
+    """ Sigmoid based on sigmoid function """
     def __call__(self, x: float) -> float:
         return 1 / (1 + exp((x + self.shift) * self.factor))
